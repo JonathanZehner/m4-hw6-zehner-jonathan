4941ee8afeb836b964ffc3f2e2be0bbc7e8c1095
Jonathan Zehner <jonathan.zehner@ufl.edu>
2021-10-03 21:26:36 -0400

    Move pizza.png to root dir; Set pizza object crust property to user's response

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function getPizzaOrder() {
   // HINT: You may wish to use .toLowerCase() and .trim()
   // if the user specified 'thick' crust, add thickCrustUpcharge
   // to pizza.cost
-  // YOUR CODE HERE
+  pizza.crust = crust.toLowerCase().trim();
 
   var toppings = prompt("Please enter additional toppings (comma separated)")
   // HINT: prompt() will return an empty string "" if the user presses 'OK' without entering a value
